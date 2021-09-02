ccdf7a31cfbc97d9067fadc6fc4fd13f4940982f
Harmeet Singh <harmeet@golfdiscount.com>
2021-09-02 13:52:03 -0700

    Printing out resource_url for loggin

--- loadOrders/__init__.py
+++ loadOrders/__init__.py
@@ -2,6 +2,7 @@ import logging
 import os
 import azure.functions as func
 import net.requester as request
+import datetime
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -9,12 +10,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     req = req.get_json()
     resource_url = req['resource_url']
     resource_type = req['resource_type']
+    logging.info(resource_url)
     
     if (resource_type != 'SHIP_NOTIFY'):
         return func.HttpResponse(f'This is not a "on items shipped" webhook', status_code=400)
 
-    order_info = request.get(resource_url, None, headers={'Authorization': 'Basic M2I3MmUyOGI0ZWI1NDdhYjk3NmNjMGFjOGIxYTA2NjI6ZmUyYmJjNjRkN2RlNDI2YzhjMjk4YjQxMDdkYWM2MGE='})
+    order_info = request.get(resource_url, None, headers={'Authorization': os.environ['AUTH_CREDS']})
+    order_info = order_info.json()
 
-    logging.info(order_info.json())
-
-    return func.HttpResponse("Finished")
+    return func.HttpResponse(f"Order sheet for order {order_info['shipments'][0]['orderKey']}")
