050d5a7354e5a6122660cd3d3a93071c36093dc9
Harmeet Singh <harmeet@golfdiscount.com>
2021-10-21 14:44:27 -0700

    Checks all orders for order numbers associated with multiple orders

--- addCustomerNote/__init__.py
+++ addCustomerNote/__init__.py
@@ -8,29 +8,30 @@ import os
 import requests
 
 async def main(req: func.HttpRequest) -> func.HttpResponse:
+    # Ensure all parameters are in the request
     if "note" not in req.params or "orderNumber" not in req.params:
-        return func.HttpResponse("Please make sure to include both note & orderNumber in your query parameters", status_code=400, mimetype="text/plain")
+        return func.HttpResponse("Please make sure to include both note & orderNumber in your query parameters",
+            status_code=400,
+            mimetype="text/plain")
 
     orders = requests.get(f"https://ssapi4.shipstation.com/orders?orderNumber={req.params['orderNumber']}",
         headers={"Authorization": os.environ.get("AUTH_CREDS")}).json()
-
     tags = requests.get(f"https://ssapi.shipstation.com/accounts/listtags").json()
 
     for tag in tags:
-        if tag["name"] == "WSI":
-            wsi_tag_id = tag["tagId"]
+        if tag["name"] == "WSI": wsi_tag_id = tag["tagId"]
 
     if wsi_tag_id is None:
         return func.HttpResponse("There is no tag with the name WSI", status_code=501, mimetype="text/plain")
 
     for order in orders:
         for tag in order["tagIds"]:
-            if tag == wsi_tag_id:
-                order_info = order
+            if tag == wsi_tag_id: order_info = order
 
     if order_info is None:
         return func.HttpResponse(f"Could not find an order with a WSI tag", status_code=400, mimetype="text/plain")
 
+    order_info["customerNotes"] = "Sent to WSI"
     ss_post = requests.post("https://ssapi.shipstation.com/orders/createorder",
         json=order_info,
         headers={"Authorization": os.environ.get("AUTH_CREDS")})
