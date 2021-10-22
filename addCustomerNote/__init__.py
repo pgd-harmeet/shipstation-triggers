bedb39fcce25a3fe3adcd6de0d6e3d5cbb17f3bd
Harmeet Singh <harmeet@golfdiscount.com>
2021-10-22 12:05:33 -0700

    Added additional logging and removed incorrect function response

--- addCustomerNote/__init__.py
+++ addCustomerNote/__init__.py
@@ -6,9 +6,11 @@ order base order number
 import azure.functions as func
 import os
 import requests
+import logging
 
 async def main(req: func.HttpRequest) -> func.HttpResponse:
     if "orderNumber" not in req.params:
+        logging.info("Request does not have the necessary parameters")
         return func.HttpResponse("Please make sure the orderNumber in your query parameter", status_code=400, mimetype="text/plain")
 
     session = requests.Session()
@@ -17,21 +19,24 @@ async def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         wsi_tag_id = get_wsi_tag(session)
     except AssertionError as e:
+        logging.error("A tag with the name WSI does not exist")
         return func.HttpResponse(str(e), status_code=400, mimetype="text/plain")
 
     orders = session.get(f"https://ssapi4.shipstation.com/orders?orderNumber={req.params['orderNumber']}").json()
 
     if orders["total"] < 1:
-        return func.HttpResponse("There are no orders for this order number", status_code=400, mimetype="text/plain")
+        logging.warn(f"There are no orders for {req.params['orderNumber']}")
+        return func.HttpResponse(f"There are no orders for {req.params['orderNumber']}", status_code=400, mimetype="text/plain")
 
     wsi_orders = []
     for order in orders["orders"]:
         try:
             if check_order(order, wsi_tag_id): wsi_orders.append(order)
         except ValueError:
-            return func.HttpResponse("The order has no tags assocaited with it", status_code=400, mimetype="text/plain")
+            logging.warn(f"Order {order['orderNumber']} has not tags associated with it")
 
     if len(wsi_orders) == 0:
+        logging.warn("There were no orders for with a WSI tag")
         return func.HttpResponse(f"Could not find an order with a WSI tag", status_code=400, mimetype="text/plain")
 
     for order in wsi_orders:
@@ -39,11 +44,13 @@ async def main(req: func.HttpRequest) -> func.HttpResponse:
         note_post = session.post("https://ssapi.shipstation.com/orders/createorder", json=order)
 
         if note_post.status_code != 200:
-            return func.HttpResponse("There was an issue adding a note to one of the orders", status_code=400, mimetype="text/plain")
+            logging.error("There was an issue adding a note to an order")
+            return func.HttpResponse("There was an issue adding a note to an order", status_code=400, mimetype="text/plain")
 
     session.close()
 
-    return func.HttpResponse("Successfully added note", status_code=200, mimetype="text/plain")
+    logging.info(f"Successfully added a note to order {req.params['orderNumber']}")
+    return func.HttpResponse(f"Successfully added a note to order {req.params['orderNumber']}", status_code=200, mimetype="text/plain")
 
 def get_wsi_tag(session: requests.Session()) -> int:
     """
